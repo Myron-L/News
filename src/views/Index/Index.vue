74eb800cd23b17e83ec1bf9a5dbb4991175fc686
Myron-L <820427037@qq.com>
2018-11-15 18:36:56 +0800

    创建项目

---
+++ src/views/Index/Index.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="page">
+
+    </div>
+</template>
+
+<script>
+/* eslint-disable */
+
+export default {
+    name: '',
+    components: {},
+    data() {
+        return {}
+    },
+    methods: {},
+    created() {
+
+    },
+    mounted() {
+
+    }
+}
+
+</script>
+<style lang="scss" scoped>
+
+</style>
